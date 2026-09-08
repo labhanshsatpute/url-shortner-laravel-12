d00470d482ec9d1dc05180ed1e9ebac2c05daeba
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2026-09-09 01:27:06 +0530

    Permission setup done

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUuids, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
